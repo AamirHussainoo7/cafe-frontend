9c5f04aa4b37e9ff0d85cc2abf620a71aea845a6
AamirHussainoo7 <aamithussain.786@gmail.com>
2025-07-10 16:15:39 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,31 @@ function App() {
   
 
  return(
- <div>
-    <h1>HUS Cafe</h1>
-    <Register/>
-    <h3>This is Footer</h3>
+  <div
+      style={{
+        fontFamily: 'Arial, sans-serif',
+        padding: '20px',
+        backgroundColor: '#f0f2f5',
+        minHeight: '100vh',
+        textAlign: 'center',
+      }}
+    >
+      <h1 style={{ color: '#333', marginBottom: '30px' }}>HUS Cafe</h1>
 
- </div>
- )
+      <Register />
+
+      <h3
+        style={{
+          marginTop: '40px',
+          color: '#888',
+          fontWeight: 'normal',
+          fontSize: '14px',
+        }}
+      >
+        This is Footer
+      </h3>
+    </div>
+  );
 }
 
-export default App
+export default App;
